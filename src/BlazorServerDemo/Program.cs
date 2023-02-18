4a580cf5de5f7883a7d823e8a8b8b41d3ed8ff7e
Christopher Homer <christopher.homer.75@gmail.com>
2023-02-18 18:08:16 +0000

    2 way binding example

--- src/BlazorServerDemo/Program.cs
+++ src/BlazorServerDemo/Program.cs
@@ -1,6 +1,4 @@
 using BlazorServerDemo.Data;
-using Microsoft.AspNetCore.Components;
-using Microsoft.AspNetCore.Components.Web;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +6,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+
 
 var app = builder.Build();
 
